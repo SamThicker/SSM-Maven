3858453895ad9b2948a0862e0f2f4979a030c002
wunaifu <1278022159@qq.com>
2018-08-09 15:20:43 +0800

    新数据库备份文件

--- starry.sql
+++ starry.sql
@@ -16,14 +16,29 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`starry` /*!40100 DEFAULT CHARACTER SET 
 
 USE `starry`;
 
+/*Table structure for table `test_tab` */
+
+DROP TABLE IF EXISTS `test_tab`;
+
+CREATE TABLE `test_tab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) DEFAULT NULL,
+  `age` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `test_tab` */
+
+insert  into `test_tab`(`id`,`name`,`age`) values (1,'张三',22),(2,'李四',10);
+
 /*Table structure for table `user_tab` */
 
 DROP TABLE IF EXISTS `user_tab`;
 
 CREATE TABLE `user_tab` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(15) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
+  `phone` varchar(15) NOT NULL,
+  `password` varchar(32) NOT NULL,
   `nickname` varchar(20) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `gender` tinyint(4) DEFAULT NULL,
@@ -38,11 +53,11 @@ CREATE TABLE `user_tab` (
   `signature` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user_tab` */
 
-insert  into `user_tab`(`id`,`phone`,`password`,`nickname`,`name`,`gender`,`age`,`habit`,`birthday`,`job`,`address`,`weigh`,`height`,`xingZuo`,`signature`) values (1,'1','1','1','1',1,1,'1','1','1','1','1','1','1','1');
+insert  into `user_tab`(`id`,`phone`,`password`,`nickname`,`name`,`gender`,`age`,`habit`,`birthday`,`job`,`address`,`weigh`,`height`,`xingZuo`,`signature`) values (1,'1','1','1','da',0,23,'1','1','1','1','1','1','1','1'),(2,'2','2',NULL,'福',1,23,NULL,'2018-01-29',NULL,'插入新地址',NULL,NULL,NULL,NULL),(3,'123','123456',NULL,'福',1,23,NULL,'2018-01-29',NULL,'插入新地址',NULL,NULL,NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
